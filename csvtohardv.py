5afc821e4ff10def252a3b7f7a23208f15fc665b
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-06-13 15:18:03 +0100

    added autogeneration of partial format string for buffer

--- csvtohardv.py
+++ csvtohardv.py
@@ -7,9 +7,6 @@
 
 # 1. function takes in array (of column names from csv) and optionally column name for explanation, automatically generating permutations # done
 # 2. output this to a neovim buffer for editing and add a format sequence to the end of each line
-# https://stackoverflow.com/questions/10129214/opening-vi-from-python
-# https://parezcoydigo.wordpress.com/2012/08/11/call-vim-from-inside-a-python-script/
-# https://github.com/neovim/pynvim
 # 3. save text from this neovim buffer, performing the function writing the hardv format 
 # this function takes in csv file, output file, format string, args from column_combinatorics ([["1", "2"], ["3", "4"]] or [["1", "2"], "5"])
 # for a given card, check whether a question is not output to a file if it is already present, unless the answer has been changed, in which case replace it
@@ -19,6 +16,7 @@
 from sys import argv
 from itertools import permutations
 import csv
+import tempfile, subprocess
 
 def column_combinatorics(array, explanation=None):
     array_only_lists = False
@@ -27,36 +25,52 @@ def column_combinatorics(array, explanation=None):
             array_only_lists = True
     if array_only_lists == True:
         if len(array) != 2:
-            raise ValueError("The array should contain exactly two sublists")
+            raise ValueError("The array should contain exactly two sub_perm_lists")
         primary_perms = permutations(array, len(array))
 
         #use case only for arrays with only subarrays
         #returns list
+        final_perm_string = ""
         for primary_perm in list(primary_perms):
             secondary_perms = permutations(primary_perm[0], 1)
             for secondary_perm in list(secondary_perms):
-                finallist = [secondary_perm[0]]
-                for sublist in primary_perm[1:]:
-                    for subsublist in sublist:
-                        finallist.append(subsublist)
+                final_perm_list = [secondary_perm[0]]
+                for sub_perm_list in primary_perm[1:]:
+                    for sub_sub_perm_list in sub_perm_list:
+                        final_perm_list.append(sub_sub_perm_list)
 #                        if explanation is not None:
-#                            finallist.append([explanation])
+#                            final_perm_list.append([explanation])
 #                            print(explanation)
-                print(finallist)
+#                print(final_perm_list)
+                final_perm_string += str(final_perm_list) + ", formatstring='\"%s\"'" + "\n"
     else:
         #use case only for arrays with no subarrays
         #returns list
         perms = permutations(array, 2)
+        final_perm_string = ""
         for permutation in list(perms):
 #            print(permutation)
-            finallist = []
-            for sublist in permutation:
-                finallist.append(sublist)
+            final_perm_list = []
+            for sub_perm_list in permutation:
+                final_perm_list.append(sub_perm_list)
             if explanation is not None:
-                finallist.append(explanation)
+                final_perm_list.append(explanation)
 #                        print(explanation)
-            print(finallist)
+#            print(final_perm_list)
+            final_perm_string += str(final_perm_list) + ", formatstring='\"%s\"'" + "\n"
+
 #   output to nvim buffer
+#    print(final_perm_string)
+
+    with tempfile.NamedTemporaryFile(suffix='csvtohardv') as temp:
+        text = open(temp.name, 'w')
+        text.write(str(final_perm_string))
+        text.close()
+        subprocess.call(['nvim', temp.name])
+        text = open(temp.name, 'r')
+        buffer_output = text.read()
+        temp.close()
+    print(buffer_output)
 
 def check_whether_in_file():
     pass
@@ -94,11 +108,11 @@ def main():
 #    output_to_hardv("/home/jcrtf/c-syntax-structures.csv", "Write the code for the C syntax structure \"%s\"", "structure", "code", "desc", mod="hello.sh")
 #    column_combinatorics([["eng.txt", "eng.aud", "eng.vid"], ["rus.txt", "rus.aud", "rus.vid"], ["ara.txt", "ara.aud", "rus.vid"]]) # inhibit use case # done
 #    column_combinatorics([["eng.txt", "eng.aud", "eng.vid"], ["rus.txt", "rus.aud", "rus.vid"]]) # desired behaviour
-#    column_combinatorics([["eng.txt", "eng.aud"], ["rus.txt", "rus.aud"]]) # desired behaviour
+    column_combinatorics([["eng.txt", "eng.aud"], ["rus.txt", "rus.aud"]]) # desired behaviour
 #    column_combinatorics(["structure", "code", "desc"]) # desired behaviour
 #    column_combinatorics(["structure", "code"]) # desired behaviour
 #    column_combinatorics(["location", "pao"], explanation="explanation") # desired behaviour
 #    column_combinatorics(["location", "pao"], "explanation") # desired behaviour
-    check_whether_in_file()
+#    check_whether_in_file()
 
 main()
