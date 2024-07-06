2d5be910580f6f31681d8bb8b42bc36e334b8c71
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2024-07-06 12:28:27 +0100

    sorted out card comparison, combination and output

--- csvtohardv.py
+++ csvtohardv.py
@@ -17,38 +17,30 @@
 from itertools import permutations
 import csv
 import tempfile, subprocess
-from re import findall, sub, split
+from re import findall, sub, split, search, DOTALL
 
-class Card:
-    def __init__(self, mod=None, nexttime=None, prevtime=None, questionfield=None, answerfield=None, additionalfields=None):
-        self.mod = mod
-        self.nexttime = nexttime
-        self.prevtime = prevtime
-        self.questionfield = questionfield
-        self.answerfield = answerfield
-        # Dictionary of additional fields
-        self.additionalfields = additionalfields
+#input_card = """NEXT	2023-09-14 19:05:49 +0100
+#PREV	2023-09-12 19:05:49 +0100
+#Q	Describe the C syntax structure coded "union NAME {
+#    ELEMENTS;
+#} [VARIABLE_NAME];".
+#A	creates a custom data type where only one instance can exist
+#STRUCTURE	union
+#%%this is the first comment
+#%%this is the second comment
+#%%
+#
+#"""
+#
+#input_untabbed = """Describe the C syntax structure coded "union NAME {
+#    ELEMENTS;
+#} [VARIABLE_NAME];"."""
+#
+#input_tabbed = """Describe the C syntax structure coded "union NAME {
+#	    ELEMENTS;
+#	} [VARIABLE_NAME];"."""
 
-    def __str__(self):
-        returnlist = []
-        if self.mod is not None:
-            returnlist.append("MOD\t" + self.mod)
-        if self.nexttime is not None:
-            returnlist.append("NEXT\t" + self.nexttime)
-        if self.prevtime is not None:
-            returnlist.append("PREV\t" + self.prevtime)
-        if self.questionfield is not None:
-            returnlist.append("Q\t" + self.questionfield)
-        if self.answerfield is not None:
-            returnlist.append("A\t" + self.answerfield)
-        if self.additionalfields is not None:
-            for key, value in self.additionalfields.items():
-                returnlist.append(key.upper() + "\t" + value)
-        returnlist.append("%%\n")
-        return "\n".join(returnlist)
-
-card1 = Card(questionfield="When was the Norman invasion of England?", answerfield="1066", additionalfields={"winningside": "Normans"})
-
+# Prepend tab to any line n > 1 in a string
 def add_tabs(instring):
     returnlist = []
     count = 0
@@ -57,7 +49,7 @@ def add_tabs(instring):
             returnlist.append(line)
         else:
             returnlist.append("\t" + line)
-        count+=1
+        count += 1
     return "\n".join(returnlist)
 
 def column_combinatorics(array, explanation=None):
@@ -82,7 +74,7 @@ def column_combinatorics(array, explanation=None):
 #                        if explanation is not None:
 #                            final_perm_list.append([explanation])
 #                            print(explanation)
-                final_perm_string += str(" ".join(final_perm_list)) + " formatstring='\"%s\"' outfile=\"\"" + "\n"
+                final_perm_string += str(" ".join(final_perm_list)) + " format_string='\"%s\"' outfile=\"\"" + "\n"
     else:
         # Use case only for arrays comprised of no subarrays
         perms = permutations(array, 2)
@@ -93,7 +85,7 @@ def column_combinatorics(array, explanation=None):
                 final_perm_list.append(sub_perm_list)
             if explanation is not None:
                 final_perm_list.append(explanation)
-            final_perm_string += str(" ".join(final_perm_list)) + " formatstring='\"%s\"' outfile=\"\"" + "\n"
+            final_perm_string += str(" ".join(final_perm_list)) + " format_string='\"%s\"' outfile=\"\"" + "\n"
 
     # Output to nvim buffer
     with tempfile.NamedTemporaryFile(suffix='csvtohardv') as temp:
@@ -106,14 +98,14 @@ def column_combinatorics(array, explanation=None):
         temp.close()
     return(buffer_output)
 
-def check_whether_in_file():
-    pass
 
-def output_to_hardv(infile, selected_column_names, formatstring='"%s"', mod=None, outfile="/dev/stdout"):
+# Return a list of dictionaries embodying a card
+def csv_to_dict(infile, selected_column_names, format_string='"%s"', mod=None):
+
+    input_card_list = []
+
     with open(infile, newline='') as csvfile:
         # Wipe output file
-        open(outfile, 'w').close()
-        text = open(outfile, "a")
         csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
         all_column_names = []
         for row in csvreader:
@@ -125,56 +117,150 @@ def output_to_hardv(infile, selected_column_names, formatstring='"%s"', mod=None
         if "prev" in all_column_names:
             raise ValueError("A column name may not be \"prev\"")
         for row in csvreader:
-            if mod != '':
-                text.write("MOD\t%s\n" % mod)
-            text.write("Q\t" + formatstring % row[all_column_names.index(selected_column_names[0])] + "\n")
-            text.write("A\t" + row[all_column_names.index(selected_column_names[1])] + "\n")
+            card_dict = {'MOD': mod,
+                        'question_field': add_tabs(row[all_column_names.index(selected_column_names[0])]),
+                        'format_string': format_string,
+                        'A': row[all_column_names.index(selected_column_names[1])]}
             for column_name in selected_column_names[2:]:
-                text.write(column_name.upper() + "\t" + row[all_column_names.index(column_name)] + "\n")
-            text.write("%%\n\n")
-        text.close()
+                card_dict[column_name.upper()] = row[all_column_names.index(column_name)]
+            input_card_list.append(card_dict)
     csvfile.close()
+    return input_card_list
 
 
-def main():
-#    column_combinatorics([["eng.txt", "eng.aud", "eng.vid"], ["rus.txt", "rus.aud", "rus.vid"], ["ara.txt", "ara.aud", "rus.vid"]]) # inhibit use case # done
-#    column_combinatorics([["eng.txt", "eng.aud", "eng.vid"], ["rus.txt", "rus.aud", "rus.vid"]]) # desired behaviour
-#    column_combinatorics([["eng.txt", "eng.aud"], ["rus.txt", "rus.aud"]]) # desired behaviour
-#    column_combinatorics(["structure", "code"]) # desired behaviour
-#    column_combinatorics(["location", "pao"], explanation="explanation") # desired behaviour
-#    column_combinatorics(["location", "pao"], "explanation") # desired behaviour
-#    check_whether_in_file()
+def hardv_card_to_dict(input_str):
 
-    buffer_output = column_combinatorics(["structure", "code", "desc"]) # desired behaviour
-#    buffer_output='code structure formatstring=\'Name the C syntax structure coded "%s"\' outfile="/dev/stdout"'
+    line_list = []
+    card = {}
 
-    lineiterator = buffer_output.splitlines()
-    for line in lineiterator:
-        keywords = findall("\\w+=\".+?\"|\\w+=\'.+?\'|\\w+=[\\S^\'\"]+", line)
-        non_keywords = sub("\\w+=\".+?\"|\\w+=\'.+?\'|\\w+=[\\S^\'\"]+", '', line).split()
+    count = 0
+    for line in input_str.split("\n"):
+        if count == 0:
+            line_list.append(line + "\n")
+        else:
+            if bool(search("^[A-Z]+\t|^%%", line)):
+                line_list.append(line + "\n")
+            else:
+                line_list[-1] = line_list[-1] + line + "\n"
+        count += 1
 
-        try:
-            outfile_full_arg = [ match for match in keywords if "outfile=" in match ][0]
-            outfile_quoted_arg = split("=", outfile_full_arg, 1)[1]
-            outfile_arg = str(outfile_quoted_arg.replace("\"", "", 1).replace("\"", "", len(outfile_quoted_arg)))
-        except IndexError:
-            outfile_arg='/dev/stdout'
-    
-        try:
-            mod_full_arg = [ match for match in keywords if "mod=" in match ][0]
-            mod_quoted_arg = split("=", mod_full_arg, 1)[1]
-            mod_arg = str(mod_quoted_arg.replace("\"", "", 1).replace("\"", "", len(mod_quoted_arg)))
-        except IndexError:
-            mod_arg=''
-    
-        try:
-            formatstring_full_arg = [ match for match in keywords if "formatstring=" in match ][0]
-            formatstring_quoted_arg = split("=", formatstring_full_arg, 1)[1]
-            formatstring_arg = str(formatstring_quoted_arg.replace("\'", "", 1).replace("\'", "", len(formatstring_quoted_arg)))
-        except IndexError:
-            formatstring_arg='%s'
+    commentno=1
+    for field in line_list:
+#        print(field)
+        if bool(search("^[A-Z]+\t", field)):
+            key = findall("^[A-Z]+", field)[0]
+            value = findall("(?<=[A-Z]\t).*", field, DOTALL)[0]
+# If newline is causing issues, look at lines below
+            if value[-1] == "\n":
+                value = value[:-1]
+#                print(value)
+            if key == "Q":
+                if bool(search("\".*\"", field, DOTALL)):
+                    question_field_var = findall("(?<=\").*(?=\")", field, DOTALL)[0]
+                    card["question_field"] = question_field_var
+                    # Split string and insert %s
+                    fmt_str_list = value.split(question_field_var)
+                    fmt_str_list.insert(1,"%s")
+                    card["format_string"] = "".join(fmt_str_list)
+                else:
+                    card["question_field"] = value
+                    card["format_string"] = "%s"
+            else:
+                card[key] = value
+        elif bool(search("^%%\n", field)):
+            card["terminator"] = field
+        elif bool(search("^%%", field)):
+            card["comment" + str(commentno)] = field.rstrip()
+            commentno+=1
+        else:
+            raise ValueError
 
-        output_to_hardv("/home/jcrtf/archives/flashcards/c-syntax-structures.csv", non_keywords, formatstring=formatstring_arg, mod=mod_arg, outfile=outfile_arg)
+    return card
 
-#main()
-print(str(card1))
+def hardv_file_to_list(input_file):
+
+    with open(input_file, newline='') as infile:
+        file_content = infile.read()
+        card_list = split("%%\n\n", file_content)
+        counter = 0
+        for card in card_list[:-1]:
+            # Add support for %%\n
+            card_list[counter] = card + "%%\n"
+            counter += 1
+        card_list.remove('')
+    return card_list
+
+#buffer_output = column_combinatorics(["structure", "code", "desc"]) # desired behaviour
+buffer_output='code desc format_string=\'Describe the C syntax structure coded "%s"\' outfile="/home/jcrtf/csvtohardv/code-to-structure.fc"'
+lineiterator = buffer_output.splitlines()
+for line in lineiterator:
+
+    # Read arguments from buffer_output
+    keywords = findall("\\w+=\".+?\"|\\w+=\'.+?\'|\\w+=[\\S^\'\"]+", line)
+    non_keywords = sub("\\w+=\".+?\"|\\w+=\'.+?\'|\\w+=[\\S^\'\"]+", '', line).split()
+
+    try:
+        outfile_full_arg = [ match for match in keywords if "outfile=" in match ][0]
+        outfile_quoted_arg = split("=", outfile_full_arg, 1)[1]
+        outfile_arg = str(outfile_quoted_arg.replace("\"", "", 1).replace("\"", "", len(outfile_quoted_arg)))
+    except IndexError:
+        outfile_arg='/dev/stdout'
+
+    try:
+        mod_full_arg = [ match for match in keywords if "mod=" in match ][0]
+        mod_quoted_arg = split("=", mod_full_arg, 1)[1]
+        mod_arg = str(mod_quoted_arg.replace("\"", "", 1).replace("\"", "", len(mod_quoted_arg)))
+    except IndexError:
+        mod_arg=''
+
+    try:
+        format_string_full_arg = [ match for match in keywords if "format_string=" in match ][0]
+        format_string_quoted_arg = split("=", format_string_full_arg, 1)[1]
+        format_string_arg = str(format_string_quoted_arg.replace("\'", "", 1).replace("\'", "", len(format_string_quoted_arg)))
+    except IndexError:
+        format_string_arg='%s'
+
+    generated_list = csv_to_dict("/home/jcrtf/projects/csvtohardv/c-syntax-structures.csv", non_keywords, format_string=format_string_arg, mod=mod_arg)
+#    Generate the list of existing cards
+    existing_list = []
+    for card in hardv_file_to_list(outfile_arg):
+        existing_list.append(hardv_card_to_dict(card))
+
+    combined_list = []
+    question_field_list = [existing_card['question_field'] for existing_card in existing_list]
+
+#    print(generated_list)
+#    print(existing_list)
+
+    for card in generated_list:
+        if card['question_field'] in question_field_list:
+            card_no = question_field_list.index(card['question_field'])
+            combined_list.append(existing_list[card_no] | card)
+#            print(existing_list[card_no])
+#            print(card)
+        else:
+            combined_list.append(card)
+#            pass
+
+    # Wipe output file
+    open(outfile_arg, 'w').close()
+    text = open("/dev/stdout", "a")
+    for item in combined_list:
+#        print(item.keys())
+        if item['MOD'] != '':
+            text.write("MOD\t" + item['MOD'] + "\n")
+        if 'NEXT' in item.keys():
+            text.write("NEXT\t" + item['NEXT'] + "\n")
+        if 'PREV' in item.keys():
+            text.write("PREV\t" + item['PREV'] + "\n")
+        text.write("Q\t" + item['format_string'] % item['question_field'] + "\n")
+        text.write("A\t" + item['A'] + "\n")
+        # Insert all other allcaps values
+        for x in item.keys():
+            if bool(search("^[A-Z]+", x)) and x != 'MOD' and x != 'A' and x != 'NEXT' and x != 'PREV':
+                text.write(x + "\t" + item[x] + "\n")
+        # Insert all comments
+        for x in item.keys():
+            if bool(search("^comment.+", x)):
+                text.write(item[x] + "\n")
+        text.write('%%\n\n')
